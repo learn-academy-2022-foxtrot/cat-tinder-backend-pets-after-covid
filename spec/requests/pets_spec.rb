a5e8de2ccb2e08a8f8c9eaa85c94bf3f4314761b
rhodesmt <mrhodes415@gmail.com>
2022-10-25 16:11:55 -0700

    Completed Stretch. MVP Complete

--- spec/requests/pets_spec.rb
+++ spec/requests/pets_spec.rb
@@ -53,9 +53,8 @@ RSpec.describe "Pets", type: :request do
     end
   end
 
-
-
   describe "cannot create a pet without valid attributes" do
+
     it "doesn't create a pet without a name" do
      pet_params = {
         pet: {
@@ -67,6 +66,127 @@ RSpec.describe "Pets", type: :request do
      json = JSON.parse(response.body)
      expect(json["name"]).to include "can't be blank"
      end
+
+    it "doesn't create a pet without a age" do
+     pet_params = {
+        pet: {
+           name: 'Bear', enjoys: 'biting stuff', image: 'url'
+       }
+     }
+     post '/pets', params: pet_params
+     expect(response.status).to eq(422)
+     json = JSON.parse(response.body)
+     expect(json["age"]).to include "can't be blank"
+     end
+
+    it "doesn't create a pet without a enjoys" do
+     pet_params = {
+        pet: {
+           name: 'Bear', age: 3, image: 'url'
+       }
+     }
+     post '/pets', params: pet_params
+     expect(response.status).to eq(422)
+     json = JSON.parse(response.body)
+     expect(json["enjoys"]).to include "can't be blank"
+     end
+
+    it "doesn't create a pet without a image" do
+     pet_params = {
+        pet: {
+           name: 'Bear', age: 3, enjoys: 'biting stuff'
+       }
+     }
+     post '/pets', params: pet_params
+     expect(response.status).to eq(422)
+     json = JSON.parse(response.body)
+     expect(json["image"]).to include "can't be blank"
+     end
+  end
+
+  describe "cannot update a pet without valid attributes" do
+
+    it "doesn't update a pet without a name" do
+      Pet.create(id: 1, name: 'Bear', age: 3, enjoys: 'Lots of pets and snacks', image: 'URL')
+
+      patch '/pets/1',  
+        params:{
+          pet:{
+            id: 1, name: '', age: 3, enjoys: 'Lots of pets and snacks', image: 'URL'}}
+
+
+      # expect(response.status).to eq(200)
+      pet = Pet.find(1)
+     expect(response.status).to eq(422)
+     json = JSON.parse(response.body)
+     expect(json["name"]).to include "can't be blank"
+     end
+
+    it "doesn't update a pet without a age" do
+      Pet.create(id: 1, name: 'Bear', age: 3, enjoys: 'Lots of pets and snacks', image: 'URL')
+
+      patch '/pets/1',  
+        params:{
+          pet:{
+            id: 1, name: 'Bear', age: nil, enjoys: 'Lots of pets and snacks', image: 'URL'}}
+
+
+      # expect(response.status).to eq(200)
+      pet = Pet.find(1)
+     expect(response.status).to eq(422)
+     json = JSON.parse(response.body)
+     expect(json["age"]).to include "can't be blank"
+     end
+
+    it "doesn't update a pet without an enjoys section" do
+      Pet.create(id: 1, name: 'Bear', age: 3, enjoys: 'Lots of pets and snacks', image: 'URL')
+
+      patch '/pets/1',  
+        params:{
+          pet:{
+            id: 1, name: 'Bear', age: 3, enjoys: '', image: 'URL'}}
+
+
+      # expect(response.status).to eq(200)
+      pet = Pet.find(1)
+     expect(response.status).to eq(422)
+     json = JSON.parse(response.body)
+     expect(json["enjoys"]).to include "can't be blank"
+     end
+     
+    it "doesn't update a pet with an enjoys section less than 10 characters" do
+      Pet.create(id: 1, name: 'Bear', age: 3, enjoys: 'Lots of pets and snacks', image: 'URL')
+
+      patch '/pets/1',  
+        params:{
+          pet:{
+            id: 1, name: 'Bear', age: 3, enjoys: 'Food', image: 'URL'}}
+
+
+      # expect(response.status).to eq(200)
+      pet = Pet.find(1)
+     expect(response.status).to eq(422)
+     json = JSON.parse(response.body)
+     expect(json["enjoys"]).to include "is too short (minimum is 10 characters)"
+     end
+     
+
+    it "doesn't update a pet without a image" do
+      Pet.create(id: 1, name: 'Bear', age: 3, enjoys: 'Lots of pets and snacks', image: 'URL')
+
+      patch '/pets/1',  
+        params:{
+          pet:{
+            id: 1, name: 'Bear', age: 3, enjoys: 'Lots of pets and snacks', image: ''}}
+
+
+      # expect(response.status).to eq(200)
+      pet = Pet.find(1)
+     expect(response.status).to eq(422)
+     json = JSON.parse(response.body)
+     expect(json["image"]).to include "can't be blank"
+     end
+
   end
 
 end
